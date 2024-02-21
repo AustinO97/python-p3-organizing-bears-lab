67b25487337ab58279014f240f5c793c151a51fa
austin <austinoblow97@gmail.com>
2024-02-21 13:26:27 -0600

    last one

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -19,7 +19,7 @@ select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = ""
         bears.age
     FROM bears
     WHERE bears.alive = 1
-    ORDER BY bears.age
+    ORDER BY bears.age ASC
 """
 
 select_oldest_bear_and_returns_name_and_age = """
@@ -27,12 +27,14 @@ select_oldest_bear_and_returns_name_and_age = """
         bears.name
         bears.age
     FROM bears
-    ORDER BY bears.age DESC LIMIT 1
+    ORDER BY bears.age DESC 
+    LIMIT 1
 """
 select_youngest_bear_and_returns_name_and_age = """
     SELECT 
         bears.name
         bears.age
     FROM bears
-    ORDER BY bears.age LIMIT 1
+    ORDER BY bears.age ASC
+    LIMIT 1
 """
