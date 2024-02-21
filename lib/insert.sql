b7787e5e676b3f7f64d0128008c859a8fa4d0b74
austin <austinoblow97@gmail.com>
2024-02-21 12:40:16 -0600

    lab

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1, 'Mr. Chocolate', 4, 'M', 'Brown', 'Spicy', 0)
-INSERT INTO bears VALUES (2, 'Rowdy', 2, 'M', 'Brown', 'Happy', 1)
-INSERT INTO bears VALUES (3, 'Tabitha', 3, 'F', 'Black', 'Fiesty', 0)
-INSERT INTO bears VALUES (4, 'Sergeant Brown', 8, 'M', 'Brown', 'Outgoing', 0)
-INSERT INTO bears VALUES (5, 'Melissa', 5, 'F', 'Brown', 'Sad', 1)
-INSERT INTO bears VALUES (6, 'Grinch', 8, 'M', 'Brown', 'Mad', 1)
-INSERT INTO bears VALUES (7, 'Wendy', 5, 'F', 'Brown', 'Proud', 0)
-INSERT INTO bears VALUES (8, NULL, 20, 'F', 'Brown', 'aggressive', 0)
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
